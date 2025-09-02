b30b0dc3cd65615c9b24258e5f9034a964cd1ff8
Daniel Boroujerdi <274674+DBoroujerdi@users.noreply.github.com>
2025-09-02 22:03:43 +0100

    fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
 import { RootLayout } from "@/routes/layout";
 import { HomePage } from "@/routes/home";
